138f3590a75d70107f6bda726cb012c5a4ea9a7f
abdelrhmanehab10 <abdelrhmanehab047@gmail.com>
2023-11-26 18:44:46 +0200

    set metadata home desc

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,13 @@ import Counters from "@/app/_components/counter/counters";
 import Price from "@/app/_components/price/price";
 import Application from "@/app/_components/application/application";
 import MCQ from "@/app/_components/mcq/mcq";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "الرئيسية | سيرفر عرب ليونز",
+  description: `سيرفر ليونز تي في أقوي سيرفر لمشاهدة المباريات والمحتوي الترفيهي في الشرق الأوسط, يحتوي علي أكثر من 200 تيرا افلام ومسلسلات وقنوات ممتازة بجودات مختلفة
+  نعمل في ليونز تي في علي توفير أفضل تجربة للمستخدم, فنقوم بتقسيم...`,
+};
 
 export default function Home() {
   // var Tawk_API = Tawk_API || {},
